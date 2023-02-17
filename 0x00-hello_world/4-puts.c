f212a4d9f198628a21b483dd8a3c77d2b6c89a36
Cklevor <cleverncube23@gmail.com>
2023-02-16 19:04:11 -0800

    0x00-hello_world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
-/*
- * main - a program that prints a line with puts function
- *
- * Return: 0 
+/**
+ * Description: main - Prints out a string to stdout.
+ * Return: 0 if success.
  */
-int main (void)
+int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
