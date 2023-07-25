5a020ba166f31e6aaeba70c73d67741acde29b9a
Cleverncube23 <122821028+Cleverncube23@users.noreply.github.com>
2023-07-25 04:53:11 +0200

    Update string1.c

--- simple_shell/string1.c
+++ simple_shell/string1.c
@@ -7,19 +7,19 @@
  *
  * Return: pointer to destination
  */
-char *_strcpy(char *dest, char *src)
+char *_strcpy(char *dest, const char *src)
 {
 	int i = 0;
 
-	if (dest == src || src == 0)
-		return (dest);
+	if (dest == src || src == NULL)
+		return dest;
 	while (src[i])
 	{
 		dest[i] = src[i];
 		i++;
 	}
-	dest[i] = 0;
-	return (dest);
+	dest[i] = '\0';
+	return dest;
 }
 
 /**
@@ -34,15 +34,14 @@ char *_strdup(const char *str)
 	char *ret;
 
 	if (str == NULL)
-		return (NULL);
+		return NULL;
 	while (*str++)
 		length++;
 	ret = malloc(sizeof(char) * (length + 1));
 	if (!ret)
-		return (NULL);
-	for (length++; length--;)
-		ret[length] = *--str;
-	return (ret);
+		return NULL;
+	_strcpy(ret, str - length - 1);
+	return ret;
 }
 
 /**
@@ -51,17 +50,10 @@ char *_strdup(const char *str)
  *
  * Return: Nothing
  */
-void _puts(char *str)
+void _puts(const char *str)
 {
-	int i = 0;
-
-	if (!str)
-		return;
-	while (str[i] != '\0')
-	{
-		_putchar(str[i]);
-		i++;
-	}
+	while (*str)
+		_putchar(*str++);
 }
 
 /**
@@ -83,5 +75,5 @@ int _putchar(char c)
 	}
 	if (c != BUF_FLUSH)
 		buf[i++] = c;
-	return (1);
+	return 1;
 }
