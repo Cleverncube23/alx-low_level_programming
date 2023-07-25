2cd60b10c7c1a2a8e87aac30607fae7c1a13670d
Cleverncube23 <122821028+Cleverncube23@users.noreply.github.com>
2023-07-25 04:44:16 +0200

    Update parser.c

--- simple_shell/parser.c
+++ simple_shell/parser.c
@@ -12,34 +12,7 @@ int is_cmd(info_t *info, char *path)
 	struct stat st;
 
 	(void)info;
-	if (!path || stat(path, &st))
-		return (0);
-
-	if (st.st_mode & S_IFREG)
-	{
-		return (1);
-	}
-	return (0);
-}
-
-/**
- * dup_chars - duplicates characters
- * @pathstr: the PATH string
- * @start: starting index
- * @stop: stopping index
- *
- * Return: pointer to new buffer
- */
-char *dup_chars(char *pathstr, int start, int stop)
-{
-	static char buf[1024];
-	int i = 0, k = 0;
-
-	for (k = 0, i = start; i < stop; i++)
-		if (pathstr[i] != ':')
-			buf[k++] = pathstr[i];
-	buf[k] = 0;
-	return (buf);
+	return (path && !stat(path, &st) && S_ISREG(st.st_mode));
 }
 
 /**
@@ -53,33 +26,35 @@ char *dup_chars(char *pathstr, int start, int stop)
 char *find_path(info_t *info, char *pathstr, char *cmd)
 {
 	int i = 0, curr_pos = 0;
-	char *path;
+	char buf[1024];
 
 	if (!pathstr)
 		return (NULL);
+
 	if ((_strlen(cmd) > 2) && starts_with(cmd, "./"))
 	{
 		if (is_cmd(info, cmd))
 			return (cmd);
 	}
+
 	while (1)
 	{
 		if (!pathstr[i] || pathstr[i] == ':')
 		{
-			path = dup_chars(pathstr, curr_pos, i);
-			if (!*path)
-				_strcat(path, cmd);
+			if (curr_pos > 0)
+				_snprintf(buf, sizeof(buf), "%.*s/%s", i - curr_pos, pathstr + curr_pos, cmd);
 			else
-			{
-				_strcat(path, "/");
-				_strcat(path, cmd);
-			}
-			if (is_cmd(info, path))
-				return (path);
+				_snprintf(buf, sizeof(buf), "%s", cmd);
+
+			if (is_cmd(info, buf))
+				return (_strdup(buf));
+
 			if (!pathstr[i])
 				break;
-			curr_pos = i;
+
+			curr_pos = i + 1;
 		}
+
 		i++;
 	}
 	return (NULL);
