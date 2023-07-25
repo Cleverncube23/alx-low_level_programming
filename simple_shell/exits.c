4d9eac78e36f9c29547b732a61ccd8def777c68b
Cleverncube23 <122821028+Cleverncube23@users.noreply.github.com>
2023-07-25 04:18:42 +0200

    Update exits.c

--- simple_shell/exits.c
+++ simple_shell/exits.c
@@ -1,74 +1,60 @@
 #include "shell.h"
 
 /**
- **_strncpy - copies a string
- *@dest: the destination string to be copied to
- *@src: the source string
- *@n: the amount of characters to be copied
- *Return: the concatenated string
+ * _strncpy - Copies a string with a maximum of n characters.
+ * @dest: The destination string to be copied to.
+ * @src: The source string.
+ * @n: The maximum number of characters to be copied.
+ * Return: The pointer to the destination string.
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i, j;
-	char *s = dest;
+	int i;
 
-	i = 0;
-	while (src[i] != '\0' && i < n - 1)
-	{
+	for (i = 0; src[i] != '\0' && i < n - 1; i++)
 		dest[i] = src[i];
-		i++;
-	}
-	if (i < n)
-	{
-		j = i;
-		while (j < n)
-		{
-			dest[j] = '\0';
-			j++;
-		}
-	}
-	return (s);
+
+	for (; i < n; i++)
+		dest[i] = '\0';
+
+	return dest;
 }
 
 /**
- **_strncat - concatenates two strings
- *@dest: the first string
- *@src: the second string
- *@n: the amount of bytes to be maximally used
- *Return: the concatenated string
+ * _strncat - Concatenates two strings with a maximum of n characters from src.
+ * @dest: The first string.
+ * @src: The second string.
+ * @n: The maximum number of bytes to be used.
+ * Return: The pointer to the destination string.
  */
 char *_strncat(char *dest, char *src, int n)
 {
 	int i, j;
-	char *s = dest;
 
-	i = 0;
-	j = 0;
-	while (dest[i] != '\0')
-		i++;
-	while (src[j] != '\0' && j < n)
-	{
-		dest[i] = src[j];
-		i++;
-		j++;
-	}
-	if (j < n)
-		dest[i] = '\0';
-	return (s);
+	for (i = 0; dest[i] != '\0'; i++)
+		;
+
+	for (j = 0; src[j] != '\0' && j < n; j++)
+		dest[i + j] = src[j];
+
+	dest[i + j] = '\0';
+
+	return dest;
 }
 
 /**
- **_strchr - locates a character in a string
- *@s: the string to be parsed
- *@c: the character to look for
- *Return: (s) a pointer to the memory area s
+ * _strchr - Locates a character in a string.
+ * @s: The string to be parsed.
+ * @c: The character to look for.
+ * Return: A pointer to the first occurrence of the character in the string,
+ * or NULL if the character is not found.
  */
 char *_strchr(char *s, char c)
 {
 	do {
 		if (*s == c)
-			return (s);
+			return s;
 	} while (*s++ != '\0');
 
-	return (NULL);
+	return NULL;
 }
