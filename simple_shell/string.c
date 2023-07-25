5f7f08d3bad090f8e77a581c95f562c51eaf1ac2
Cleverncube23 <122821028+Cleverncube23@users.noreply.github.com>
2023-07-25 04:51:46 +0200

    Update string.c

--- simple_shell/string.c
+++ simple_shell/string.c
@@ -6,26 +6,26 @@
  *
  * Return: integer length of string
  */
-int _strlen(char *s)
+int _strlen(const char *s)
 {
-	int i = 0;
+	int len = 0;
 
 	if (!s)
-		return (0);
+		return 0;
 
 	while (*s++)
-		i++;
-	return (i);
+		len++;
+	return len;
 }
 
 /**
- * _strcmp - performs lexicogarphic comparison of two strangs.
- * @s1: the first strang
- * @s2: the second strang
+ * _strcmp - performs lexicographic comparison of two strings.
+ * @s1: the first string
+ * @s2: the second string
  *
  * Return: negative if s1 < s2, positive if s1 > s2, zero if s1 == s2
  */
-int _strcmp(char *s1, char *s2)
+int _strcmp(const char *s1, const char *s2)
 {
 	while (*s1 && *s2)
 	{
@@ -34,10 +34,7 @@ int _strcmp(char *s1, char *s2)
 		s1++;
 		s2++;
 	}
-	if (*s1 == *s2)
-		return (0);
-	else
-		return (*s1 < *s2 ? -1 : 1);
+	return (*s1 - *s2);
 }
 
 /**
@@ -47,12 +44,16 @@ int _strcmp(char *s1, char *s2)
  *
  * Return: address of next char of haystack or NULL
  */
-char *starts_with(const char *haystack, const char *needle)
+const char *starts_with(const char *haystack, const char *needle)
 {
 	while (*needle)
-		if (*needle++ != *haystack++)
-			return (NULL);
-	return ((char *)haystack);
+	{
+		if (*needle != *haystack)
+			return NULL;
+		needle++;
+		haystack++;
+	}
+	return haystack;
 }
 
 /**
@@ -62,7 +63,7 @@ char *starts_with(const char *haystack, const char *needle)
  *
  * Return: pointer to destination buffer
  */
-char *_strcat(char *dest, char *src)
+char *_strcat(char *dest, const char *src)
 {
 	char *ret = dest;
 
@@ -70,6 +71,6 @@ char *_strcat(char *dest, char *src)
 		dest++;
 	while (*src)
 		*dest++ = *src++;
-	*dest = *src;
-	return (ret);
+	*dest = '\0';
+	return ret;
 }
