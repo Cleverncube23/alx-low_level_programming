b9bc375484429bdae51da98c99da16b5d629ed41
Cleverncube23 <122821028+Cleverncube23@users.noreply.github.com>
2023-07-25 04:30:14 +0200

    Update getinfo.c

--- simple_shell/getinfo.c
+++ simple_shell/getinfo.c
@@ -19,27 +19,25 @@ void clear_info(info_t *info)
  */
 void set_info(info_t *info, char **av)
 {
-	int i = 0;
-
 	info->fname = av[0];
 	if (info->arg)
 	{
 		info->argv = strtow(info->arg, " \t");
 		if (!info->argv)
 		{
-			info->argv = malloc(sizeof(char *) * 2);
+			info->argv = malloc(sizeof(char *));
 			if (info->argv)
 			{
 				info->argv[0] = _strdup(info->arg);
-				info->argv[1] = NULL;
+				info->argc = 1;
 			}
 		}
-		for (i = 0; info->argv && info->argv[i]; i++)
-			;
-		info->argc = i;
-
-		replace_alias(info);
-		replace_vars(info);
+		else
+		{
+			info->argc = count_words(info->arg, " \t");
+			replace_alias(info);
+			replace_vars(info);
+		}
 	}
 }
 
@@ -57,14 +55,11 @@ void free_info(info_t *info, int all)
 	{
 		if (!info->cmd_buf)
 			free(info->arg);
-		if (info->env)
-			free_list(&(info->env));
-		if (info->history)
-			free_list(&(info->history));
-		if (info->alias)
-			free_list(&(info->alias));
+		free_list(&(info->env));
+		free_list(&(info->history));
+		free_list(&(info->alias));
 		ffree(info->environ);
-			info->environ = NULL;
+		info->environ = NULL;
 		bfree((void **)info->cmd_buf);
 		if (info->readfd > 2)
 			close(info->readfd);
